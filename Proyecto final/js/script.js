786aa2064c19da6629e47ca61159a382db348022
Dani S <107084026+danisant15@users.noreply.github.com>
2022-12-13 18:52:23 -0300

    Proyecto Final
    Mi primera página web

---
+++ Proyecto final/js/script.js
@@ -0,0 +1,15 @@
+window.alert("¡Bienvenido/a al blog Queen Forever!")
+var edad = prompt("¿Cuantos años tienes?");
+if (edad >=18) {
+        alert("Eres mayor de edad, puedes entrar.");
+    } else {
+        alert("Eres menor de edad, no puedes entrar.");
+    }
+
+function sorteo(){
+    let dni = "Ingresa tu dni"
+    let mensaje = "¡Gracias, ya estás participando del sorteo!"
+    console.log(mensaje);
+    alert(mensaje)
+}
+
